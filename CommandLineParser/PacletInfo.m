e1ca14d5149e77929db6aaa1b3e20196f73b18d7
Matteo Salvarezza <matteos@wolfram.com>
2024-03-15 15:44:04 +0100

    Turned into paclet, add usage messages, improve command line retrieval

---
+++ CommandLineParser/PacletInfo.m
@@ -0,0 +1,10 @@
+Paclet[
+	Name -> "CommandLineParser",
+	MathematicaVersion -> "13.0+",	
+	Version -> "1.0.0",
+	Description -> "Parser for command line arguments",
+	Creator -> "Matteo Salvarezza <matteos@wolfram.com>",
+	Extensions -> {
+		{"Kernel", "Root" -> "Kernel", "Context" -> {"CommandLineParser`"}}
+	}
+]
