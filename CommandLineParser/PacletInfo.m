4a5345c265945b2e9d40b45860c2b0944feb096f
Matteo Salvarezza <matteos@wolfram.com>
2024-03-18 19:20:10 +0100

    Added license to pacletinfo, gitignore, updated definition notebook

--- CommandLineParser/PacletInfo.m
+++ CommandLineParser/PacletInfo.m
@@ -4,6 +4,7 @@ PacletObject[
     "Description" -> "Parser for command line arguments",
     "PublisherID" -> "Wolfram",
     "Creator" -> "Matteo Salvarezza <matteos@wolfram.com>",
+	 "License" -> "MIT",
     "Version" -> "1.0.0",
     "WolframVersion" -> "13.0+",
     "Extensions" -> {
