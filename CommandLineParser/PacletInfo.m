9c0148a269e588ea73c2b7e02ed043b7e796d5bd
Matteo Salvarezza <matteos@wolfram.com>
2024-03-19 15:20:06 +0100

    Bumped version

--- CommandLineParser/PacletInfo.m
+++ CommandLineParser/PacletInfo.m
@@ -5,7 +5,7 @@ PacletObject[
     "PublisherID" -> "Wolfram",
     "Creator" -> "Matteo Salvarezza <matteos@wolfram.com>",
 	 "License" -> "MIT",
-    "Version" -> "1.0.1",
+    "Version" -> "1.0.2",
     "WolframVersion" -> "13.0+",
     "Extensions" -> {
       {
